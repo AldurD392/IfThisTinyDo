5433ac541e08d7a6c84aa2a2b50d32f071cea851
Adriano Di LUzio <adrianodl@hotmail.it>
2014-12-21 16:06:04 +0100

    add command example

--- IfThisTinyDo.h
+++ IfThisTinyDo.h
@@ -6,15 +6,29 @@
 enum {
     AM_IFTHISTINYDO = 6,
     TIME_SAMPLING_MS = 250,
-    // rule mask.
+
+    // Rule masks.
+    // Sensors
     SENSOR_TEMPERATURE = 0x00, //0b00
     SENSOR_HUMIDITY = 0x01, //0b01
     SENSOR_LIGHT = 0x02, //0b10
     SENSOR_VOLTAGE = 0x03, //0b11
+
+    // Expression
     EXPRESSION_LOWER = 0x00, // 0b00
     EXPRESSION_EQUAL = 0x01, // 0b01
     EXPRESSION_GREATER = 0x02, //0b10
+    
+    // (Re-)Action
     LED = 0x00 // 0b00
+
+    /*
+     * An example of operation: 
+     * - Check the light sensor (10)
+     * - Check for light lower (00) than the maximum treshold (16 ones)
+     * - When done, turn on all the three leds (00 111).
+     * 10 00 1111111111111111 00 111 0000000
+     */
 };
 
 #endif

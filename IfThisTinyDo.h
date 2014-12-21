ecc950c09c9daadccba4c43fe991c2c31ca85dca
Danilo Francati <danilofrancati@gmail.com>
2014-12-21 15:47:00 +0100

    add rule mask

--- IfThisTinyDo.h
+++ IfThisTinyDo.h
@@ -2,27 +2,19 @@
 #define IFTHISTINYDO_H
 
 
-// Rule Message, together message_t struct is used to build a real packet.
-typedef nx_struct RuleMsg {
-    nx_uint16_t sensor;
-    nx_uint16_t threshold;
-    nx_uint16_t operator;
-    nx_uint16_t action;
-} RuleMsg;
-
 // Costants.
 enum {
     AM_IFTHISTINYDO = 6,
     TIME_SAMPLING_MS = 250,
-    SENSOR_TEMPERATURE = 10,
-    SENSOR_HUMIDITY = 11,
-    SENSOR_LIGHT = 12,
-    SENSOR_VOLTAGE = 13,
-    EXPRESSION_GREATER = 20,
-    EXPRESSION_EQUAL = 21,
-    EXPRESSION_LOWER = 22,
-    ACTION_LED_ON = 30,
-    ACTION_LED_OFF = 31,
+    // rule mask.
+    SENSOR_TEMPERATURE = 0x00, //0b00
+    SENSOR_HUMIDITY = 0x01, //0b01
+    SENSOR_LIGHT = 0x02, //0b10
+    SENSOR_VOLTAGE = 0x03, //0b11
+    EXPRESSION_LOWER = 0x00, // 0b00
+    EXPRESSION_EQUAL = 0x01, // 0b01
+    EXPRESSION_GREATER = 0x02, //0b10
+    LED = 0x00 // 0b00
 };
 
 #endif
